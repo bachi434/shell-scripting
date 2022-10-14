07d97480374f6e21a8adc7de2d4adaa8b0733589
basker <bac3433@gmail.com>
2022-10-14 14:23:55 +0530

    print colors

--- print.sh
+++ print.sh
@@ -6,4 +6,6 @@ echo hello world
 
 echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0m"
 
+echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33mYELLOW\e[1;34mBLUE\e[1;35mMAGENTA\e[1;36mCYAN\e[0m"
+
 
